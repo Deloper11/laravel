69ec453aabf4d5ef349f1ab3b06cee5d6ae6a8a1
Deloper11 <sales@etatourtravel.com>
2026-02-08 06:56:08 -0800

    Create index.php as the main entry point with routing logic

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// index.php
+
+// Simple routing logic
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Load the appropriate view or API
+} else {
+    // Handle other request methods
+}
+?>
